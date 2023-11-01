7791e339ef9b2c3d50e89c5f2cbe56fc9d389ec1
GeorgiBisolnev <93096138+GeorgiBisolnev@users.noreply.github.com>
2023-11-01 18:48:29 +0200

    Adding WEB MVC project

---
+++ Villa_WEB/Models/Dto/VillaCreateDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Villa_WEB.Models.Dto
+{
+    public class VillaCreateDTO
+    {
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int SquareFoot { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageURL { get; set; }
+        public string Amenity { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
